99811b0034ab512d1cc92d6aecf915129a094a59
sthoene <stefan.thoene@ansys.com>
2020-10-01 16:50:06 +0200

    Add initial file for class to start colaboration

---
+++ scdm_scripts/cad_data_postprocessing/preprocessinglibrary.py
@@ -0,0 +1,13 @@
+#preprocessing class
+
+class Preprocessing_asp():
+    def stitch(self):
+
+    def check_geometry_update(self):
+
+    def check_volume_conflict(self):
+
+    def resolve_volume_conflict(self):
+
+    def create_named_selection(self):
+
