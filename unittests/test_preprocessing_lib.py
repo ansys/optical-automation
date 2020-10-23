a35ee780f06c3151ce7b52baa91bc366476b7d1a
Maksim Beliaev <beliaev.m.s@gmail.com>
2020-10-23 17:17:59 +0200

    make debug connection working

--- unittests/test_preprocessing_lib.py
+++ unittests/test_preprocessing_lib.py
@@ -1,13 +1,15 @@
 import unittest
-#from SPEOS_scripts.ASP_start_Libary.ASPcontrol import SCDMControl
 import sys
+
 sys.path.append(r"D:\Git\AnsysAutomation\SCLib")
+sys.path.append(r"C:\git\ansys_automation\SCLib")
 from SCDMControl import SCDMControl
 
 class PreprocessingTest(unittest.TestCase):
-    scdm = SCDMControl(graphical_mode=True)
+    scdm = SCDMControl(graphical_mode=True, version=211, api_version="V20")
+
     def __init__(self, args):
-        super().__init__(args)
+        unittest.TestCase.__init__(self, args)
         self.scdm = PreprocessingTest.scdm
 
     @classmethod
@@ -18,20 +20,14 @@ class PreprocessingTest(unittest.TestCase):
     def tearDownClass(cls):
         cls.scdm.close_spaceclaim_session()
 
-
     def test_preprocessing(self):
-        #self.assertEqual(True, False)
-        print(dir(self.scdm.scdm_api.Document))
-        print(dir(self.scdm.scdm_api.Scripting))
-        self.scdm.import_and_save_cad_in_spaceclaim("input\poor_geom.scdoc", "input", "poor_geom2")
-        # self.scdm.scdm_api.WriteBlock.ExecuteTask("Open the document",
-        #                                      lambda: self.scdm.scdm_api.Document.Open(r"input\poor_geom.scdoc"))
-        # sc_doc = self.scdm.scdm_api.Window.ActiveWindow.Document
-        # self.scdm.scdm_api.WriteBlock.ExecuteTask("SaveAs the document",
-        #                                      lambda: self.scdm.scdm_api.Document.SaveAs(sc_doc, r"input\poor_geom2.scdoc"))
+        # self.assertEqual(True, False)
 
-        #self.scdm.scdm_api.Api.DocumentOpen.Execute(r"input\poor_geom.scdoc")
-        #self.scdm.scdm_api.Api.DocumentSave.Execute(r"input\poor_geom2.scdoc")
+        import_settings = self.scdm.scdm_api.ImportOptions.Create()
+
+        self.scdm.send_command(self.scdm.scdm_api.Document.Open, r"input\poor_geom.scdoc", import_settings)
+        sc_doc = self.scdm.scdm_api.Window.ActiveWindow.Document
+        self.scdm.send_command(self.scdm.scdm_api.Document.SaveAs, sc_doc, r"input\poor_geom2.scdoc")
 
 
 if __name__ == '__main__':
