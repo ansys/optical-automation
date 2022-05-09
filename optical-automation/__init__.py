40ae31316275e4c68387dc05e033f126763dc82e
Pengyuan Lu <pengyuan.lu@ansys.com>
2022-05-09 16:37:02 +0100

    implement  pyproject.toml

---
+++ optical-automation/__init__.py
@@ -0,0 +1,6 @@
+"""
+Library where you can find functionalities and tools for Speos, Zemax and Lumerical automations and interoperations.
+"""
+
+__version__ = "0.0.1"
+__authors__ = "Stefan Thoene, Pengyuan Lu"
