14070494a8a42b729789e7d14b171dc2db9dfd00
Pengyuan LU <89462462+pluAtAnsys@users.noreply.github.com>
2022-06-20 17:19:30 +0100

    Update scdm_CI.bat

--- scdm_CI.bat
+++ scdm_CI.bat
@@ -1,4 +1,4 @@
 cd C:\actions-runner\_work\optical-automation\optical-automation
 "C:\\Program Files\\ANSYS Inc\\v221\\scdm\\SpaceClaim.exe" /RunScript="C:\actions-runner\_work\optical-automation\optical-automation\tests\workflows\test_01_run_material_application.py" /Headless=True /Splash=False /Welcome=False /ExitAfterScript=True /ScriptAPI=21
-REM c:\windows\system32\cmd.exe /C set >c:\temp\set.txt
+c:\windows\system32\cmd.exe /C set >c:\temp\set.txt
 REM set >c:\temp\set.txt
