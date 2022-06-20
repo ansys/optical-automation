a356bedd71a2768db4cad864c7ad0baaae8b35a2
Pengyuan LU <89462462+pluAtAnsys@users.noreply.github.com>
2022-06-20 15:42:10 +0100

    Update test_01_material_application.py

--- tests/test_01_material_application.py
+++ tests/test_01_material_application.py
@@ -8,6 +8,7 @@ import subprocess
 SCDM_VERSION = 222  # version of SCDM you want to test
 
 # Code
+launcher = r"C:\Program Files\ANSYS Inc\v221\Optical Products\Speos\bin\AnsysSpeosLauncher.exe"
 scdm_install_dir = r"C:\Program Files\ANSYS Inc\v222\scdm" #get_scdm_install_location(SCDM_VERSION)
 speos_path = os.path.join(os.path.dirname(scdm_install_dir), "Optical Products", "Speos", "Bin", "SpeosSC.Manifest.xml")
 os.chdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
@@ -36,7 +37,7 @@ class TestMaterialAPI:
         scdm_script_path = os.path.join(self.local_path, "workflows", "test_01_run_material_application.py")
         print("Start SPEOS to generate JSON file for tests")
         command = [
-            r"C:\Program Files\ANSYS Inc\v221\Optical Products\Speos\bin\AnsysSpeosLauncher.exe"
+            launcher,
             scdm_exe,
             r"/AddInManifestFile={}".format(speos_path),
             r"/RunScript={}".format(scdm_script_path),
