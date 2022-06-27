bbb657f015ddeacf7275ec11da9e0d79a2bd7ab7
Pengyuan Lu <pengyuan.lu@ansys.com>
2022-06-27 15:03:57 +0100

    add __init__.py to the tests folder refactor and tests directory

--- tests/test_07_VRLab_export.py
+++ tests/test_07_VRLab_export.py
@@ -4,7 +4,7 @@ import subprocess
 
 import pytest
 
-from tests.workflows.test_07_run_VRLab_export import unittest_run
+from .workflows.test_07_run_VRLab_export import unittest_run
 
 Nvidia_GPU = False
 try:
