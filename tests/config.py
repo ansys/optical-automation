cfb06f37866792c9928d9836da95c5ac24b3e3e0
Pengyuan Lu <pengyuan.lu@ansys.com>
2022-06-27 14:38:38 +0100

    refactor get_scdm_install_location function

--- tests/config.py
+++ tests/config.py
@@ -1,5 +1,5 @@
 """Define constants and settings for tests"""
-from ansys_optical_automation.scdm_core.base import get_scdm_install_location
+from ansys_optical_automation.scdm_core.utils import get_scdm_install_location
 
 SCDM_VERSION = 222
 SCDM_INSTALL_DIR = get_scdm_install_location(SCDM_VERSION)
