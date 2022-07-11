261d50d690a21075fa79361735886790036855a8
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-07-11 11:53:20 +0200

    added Sphinx build info

--- doc/conf.py
+++ doc/conf.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
-project = 'Ansys Optical Automation'
+project = "Ansys Optical Automation"
 author = "Ansys Inc."
 
 
@@ -26,12 +26,12 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -39,9 +39,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+html_theme = "sphinx_book_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
