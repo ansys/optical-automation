81f3c4fda78d11dced99d6dad10c9d5d6a8a4715
Stefan Thöne <stefan.thoene@ansys.com>
2022-09-07 16:51:04 +0200

    remove comment cleanup

--- tests/test_09_xmp_viewer.py
+++ tests/test_09_xmp_viewer.py
@@ -30,7 +30,7 @@ class TestRayfileAPI:
 
         On fail will report traceback with lines where the code failed.
         """
-        # self.clean_results(self)
+        self.clean_results(self)
 
         print("\n\n\n\n\n###############################")
         print(self.results.get("error", "All tests are successful."))
