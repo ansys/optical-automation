c7e47ac5ca39d67c98c44994e2ca7c5c8b27d6ee
Pengyuan LU <89462462+pluAtAnsys@users.noreply.github.com>
2022-09-21 11:22:54 +0100

    Lumerical stack speos (#81)
    * Implement Lumerical Stack-SPEOS interop methods
    
    * Add test cases for Lumerical Stack-SPEOS Interop
    
    * correct the log message
    
    * implement test version control from the config.py file.
    
    * implement a method to check registry key for lumerical install folder if it is not installed in the default location
    
    * style check correction
    
    Co-authored-by: plu <plu@ansys.com>

---
+++ ansys_optical_automation/lumerical_core/utils.py
@@ -0,0 +1,34 @@
+import os
+import winreg
+
+
+def get_lumerical_install_location(version):
+    """
+    Get the Lumerical installation path.
+
+    Parameters
+    ----------
+    version : int
+        Version of Lumerical in numerical format. For example, ``222`` for 2022 R2.
+
+    Returns
+    -------
+    str
+        Path of the Lumerical installation.
+
+    """
+    lumerical = r"Lumerical v" + str(version)
+    lumerical_install_dir = None
+    try:
+        a_key = winreg.OpenKey(
+            winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE),
+            os.path.join("Software", "ANSYS, Inc.", lumerical),
+            0,
+            winreg.KEY_READ,
+        )
+        lumerical_data = winreg.QueryValueEx(a_key, "installFolder")
+        lumerical_install_dir = lumerical_data[0]
+        winreg.CloseKey(a_key)
+    except EnvironmentError:
+        raise EnvironmentError("Request Lumerical is not installed or not installed in the default location")
+    return lumerical_install_dir
---
+++ tests/test_10_stack_interop.py
@@ -0,0 +1,66 @@
+import os
+
+from .config import LUMERICAL_VERSION
+from .workflows.run_test_10_stack_interop import unittest_run
+
+
+class TestStackInteropAPI:
+    """
+    Defines conditions for running unit tests in PyTest.
+    """
+
+    def setup_class(self):
+        self.local_path = os.path.dirname(os.path.realpath(__file__))
+        self.reference_file = os.path.join(
+            self.local_path, "workflows", "example_models", "test_10_stack_interop_speos_reference.coated"
+        )
+        self.results_file = os.path.join(self.local_path, "workflows", "example_models", "test_10_stack_interop.coated")
+        self.clean_results(self)  # no idea why but you have to pass there self
+
+        print("Start Lumerical to generate file for tests.")
+        unittest_run(LUMERICAL_VERSION)
+
+    def teardown_class(self):
+        """
+        Called after all tests are completed to clean the results file.
+
+        On fail will report traceback with lines where the code failed.
+        """
+        self.clean_results(self)
+
+        print("\n\n\n\n\n###############################")
+        # print(self.results.get("error", "All tests are successful."))
+        print("###############################\n\n\n\n\n")
+
+    def clean_results(self):
+        """
+        Delete results file to avoid confusion.
+        Returns:
+        """
+        if os.path.isfile(self.results_file):
+            os.remove(self.results_file)
+
+    def test_01_stack_speos_generated(self):
+        """
+        Verify ldf content loaded and coated file generated
+        Returns:
+        -------
+        None
+        """
+        assert os.path.exists(self.results_file)
+
+    def test_02_verify_generated_coated_file(self):
+        """
+        Verify speos coated file is corrected generated via comparing to a reference
+        Returns
+        -------
+        None
+        """
+
+        res_file = open(self.results_file, "r")
+        ref_file = open(self.reference_file, "r")
+        res = res_file.read()
+        ref = ref_file.read()
+        res_file.close()
+        ref_file.close()
+        assert res == ref
