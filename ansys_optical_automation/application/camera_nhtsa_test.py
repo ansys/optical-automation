908e75b6f6e953fd77ff0ba64a5a0230ab5eaba9
Stefan Thöne <stefan.thoene@ansys.com>
2022-10-06 10:18:08 +0200

    Add optislang usage

--- ansys_optical_automation/application/camera_nhtsa_test.py
+++ ansys_optical_automation/application/camera_nhtsa_test.py
@@ -82,19 +82,19 @@ def main():
         argsDict
         mode = 1
     except Exception:
-        try:
-            "batch mode for usage in OSL"
-            sys.argv
-            mode = 2
-        except Exception:
+        if len(args) == 0:
             mode = 0
+        else:
+            mode = 2
 
     if mode == 1:
         camera_model = argsDict["camera_model"]
         car_model = argsDict["car_model"]
         print(car_model + camera_model)
     elif mode == 2:
-        # This section defines the inputs which might be replaced by Optislang
+        # This section defines the inputs which might be replaced by Optislang or the user on usage
+        repo_path = os.path.join(os.getcwd(), r"Reference\optical-automation-main")
+        sys.path.append(repo_path)
         camera_model = 1
         car_model = "SUV_22R2"
         Ansys_SPEOS_file = os.path.join(os.getcwd(), "Scene_22R2.scdocx")
@@ -105,13 +105,14 @@ def main():
             position_value = 546.123  # SUV=546.123mm
             car_path = os.path.join(os.getcwd(), "ANSYS_SUV/SUV_22R2.scdocx")
         # this section is the normal script part
-        partnames = ["Berline_22R2", "SUV_22R2"]
+        partnames = []
         # Open File
         importOptions = ImportOptions.Create()
         DocumentOpen.Execute(Ansys_SPEOS_file, importOptions)
 
         import_car(car_path, position_value)
-
+        for part in GetRootPart().Components:
+            partnames.append(part.GetName())
         ###
         # Get Camera position
         ###
@@ -205,7 +206,7 @@ def main():
         camera_model = 1
 
         InputHelper.PauseAndGetInput("Please provide a camera model number (2 models availables)", camera_model)
-        partnames = ["Berline_22R2", "SUV_22R2"]  # new
+        partnames = []  # new
         car_model = "SUV_22R2"
 
         if "Berline_22R2" in car_model:
@@ -216,7 +217,8 @@ def main():
             car_path = os.path.join(os.getcwd(), "ANSYS_SUV/SUV_22R2.scdocx")
 
         import_car(car_path, position_value)
-
+        for part in GetRootPart().Components:
+            partnames.append(part.GetName())
         ###
         # Get Camera position
         ###
