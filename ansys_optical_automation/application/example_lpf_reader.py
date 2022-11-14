cfe931a25c2c3e5be18e977bda58c32025cf246a
plu <plu@ansys.com>
2022-11-14 22:30:17 +0000

    optimize sorting method

--- ansys_optical_automation/application/example_lpf_reader.py
+++ ansys_optical_automation/application/example_lpf_reader.py
@@ -151,9 +151,9 @@ def create_diopter_map(lpf_object, sequence_id, export_name, export_path, map_si
     for trace in lpf_object.sequences[sequence_id]:
         dir_out = [trace.LastDirection.Get(0), trace.LastDirection.Get(1), trace.LastDirection.Get(2)]
         vector_in = [
-            trace.vImpacts.Get(0).Get(0) - trace.vImpacts.Get(1).Get(0),
-            trace.vImpacts.Get(0).Get(1) - trace.vImpacts.Get(1).Get(1),
-            trace.vImpacts.Get(0).Get(2) - trace.vImpacts.Get(1).Get(2),
+            abs(trace.vImpacts.Get(0).Get(0) - trace.vImpacts.Get(1).Get(0)),
+            abs(trace.vImpacts.Get(0).Get(1) - trace.vImpacts.Get(1).Get(1)),
+            abs(trace.vImpacts.Get(0).Get(2) - trace.vImpacts.Get(1).Get(2)),
         ]
         dir_in = vector_normalize(vector_in)
         point_1 = [trace.vImpacts.Get(1).Get(0), trace.vImpacts.Get(1).Get(1), trace.vImpacts.Get(1).Get(2)]
@@ -169,28 +169,19 @@ def create_diopter_map(lpf_object, sequence_id, export_name, export_path, map_si
             compute_refactive_power(point_1, point_2, dir_out, dir_in),
         ]
         refractive_power.append(temp_list)
-    my_list = refractive_power
     diopter_map = MapStruct(3, 20, 2, 9, 1, map_size, map_res)
     data = np.zeros((map_res[0], map_res[1]), dtype=list)
-    step_x = (map_size[1] - (map_size[0])) / map_res[0]
-    step_y = (map_size[3] - (map_size[2])) / map_res[1]
+    step_x = (map_size[1] - map_size[0]) / map_res[0]
+    step_y = (map_size[3] - map_size[2]) / map_res[1]
     for x in range(map_res[0]):
         for y in range(map_res[1]):
             data[x, y] = []
-    for item in my_list:
-        x_values = np.arange(map_size[0], map_size[1], step_x)
-        y_values = np.arange(map_size[2], map_size[3], step_y)
-        x_values = x_values.tolist()
-        y_values = y_values.tolist()
-        x_values.append(item[1])
-        y_values.append(item[2])
-        x_values.sort()
-        y_values.sort()
-        x = map_res[0] + 1 - x_values.index(item[1])
-        y = map_res[1] + 1 - y_values.index(item[2])
-        if not (x > map_res[0] and y > map_res[1]):
-            data[x, y].append(item[3])
-
+    for item in refractive_power:
+        x_idx = math.floor((item[1] - map_size[0]) / step_x)
+        y_idx = math.floor((item[2] - map_size[2]) / step_y)
+        x = map_res[0] - x_idx
+        y = map_res[1] - y_idx
+        data[x, y].append(item[3])
     for x in range(map_res[0]):
         for y in range(map_res[1]):
             if len(data[x, y]) == 0:
@@ -208,4 +199,4 @@ my_lpf.open_file(file_name)
 my_lpf.retrieve_traces()
 xmp_size = [-my_lpf.trace_boundary[1], my_lpf.trace_boundary[1], -my_lpf.trace_boundary[2], my_lpf.trace_boundary[2]]
 create_diopter_map(my_lpf, 1, "trans", r"c:\temp", xmp_size, [50, 50])
-create_diopter_map(my_lpf, 2, "ghost", r"c:\temp", xmp_size, [180, 60])
+create_diopter_map(my_lpf, 2, "ghost", r"c:\temp", xmp_size, [50, 50])
