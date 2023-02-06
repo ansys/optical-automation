e05d75b204b883dfb69ed2b8a00a0ce2aea53ca9
plu <plu@ansys.com>
2023-02-06 17:43:03 +0000

    avoid using CreateObject

--- ansys_optical_automation/interop_process/coating_converter.py
+++ ansys_optical_automation/interop_process/coating_converter.py
@@ -2,10 +2,10 @@ import io
 import os
 import shutil
 
-from comtypes.client import CreateObject
-
-# from ansys_optical_automation.post_process.dpf_base import DataProcessingFramework
+from ansys_optical_automation.post_process.dpf_base import DataProcessingFramework
 from ansys_optical_automation.zemax_process.base import BaseZOS
+
+# from comtypes.client import CreateObject
 
 
 class CoatingConverter:
@@ -179,14 +179,14 @@ class CoatingConverter:
 
         """
         # Create the BSDF180 that combines the two coatings
-        # bsdf_viewer = DataProcessingFramework(extension=".bsdf180", application="SimpleBSDFSurfaceViewer.Application")
-        # bsdf_viewer.dpf_instance.BuildBSDF180(coating_file_1, coating_file_2)
-        # bsdf_viewer.dpf_instance.SaveFile(speos_bsdf180)
+        bsdf_viewer = DataProcessingFramework(extension=".bsdf180", application="SimpleBSDFSurfaceViewer.Application")
+        bsdf_viewer.dpf_instance.BuildBSDF180(coating_file_1, coating_file_2)
+        bsdf_viewer.dpf_instance.SaveFile(speos_bsdf180)
 
-        bsdf_viewer = CreateObject("SimpleBSDFSurfaceViewer.Application222")
-        # # Builds BSDF 180
-        bsdf_viewer.BuildBSDF180(coating_file_1, coating_file_2)
-        bsdf_viewer.SaveFile(speos_bsdf180)
+        # bsdf_viewer = CreateObject("SimpleBSDFSurfaceViewer.Application222")
+        # # # Builds BSDF 180
+        # bsdf_viewer.BuildBSDF180(coating_file_1, coating_file_2)
+        # bsdf_viewer.SaveFile(speos_bsdf180)
 
     def __check_wavelength_range(self, substrate_material, user_wavelength_min, user_wavelength_max):
         """
