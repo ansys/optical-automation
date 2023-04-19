e2c312e087db2bbdaeb9ff93dc6a0188cebca145
SandAuriol <sandrine.auriol@ansys.com>
2023-04-19 15:43:40 +0100

    Update BSDF_converter_example.py

--- ansys_optical_automation/application/BSDF_converter_example.py
+++ ansys_optical_automation/application/BSDF_converter_example.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
-
+#import sys
+#repo_path="your_dir"
+#sys.path.append(repo_path)
 from ansys_optical_automation.interop_process.BSDF_converter import BsdfStructure
 
 
