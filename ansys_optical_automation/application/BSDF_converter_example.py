4753b9c659bcbc502884a3c35269319206ba779f
SandAuriol <98887972+SandAuriol@users.noreply.github.com>
2023-04-20 09:12:22 +0100

    Bsdf converter (#142)
    * Adding a BSDF converter from Zemax BSDF to Speos BSDF
    
    * Correction of a bug for the BTDF conversion
    
    A part of the data was not converted which resulted in zero values
    
    * Emptying the matrix
    
    * to follow pep8 standard and pre-commit check
    
    * create unittest for zemax to speos bsdf file
    
    * Added the conversion of Speos BRDF to Zemax BSDF
    
    The work is still in progress and is not validated yet.
    
    * Update following pre-commit
    
    * Fixed an error on TIS
    
    * Adding support to BRDF with reflection and transmission data
    
    * Update Convert_BSDF_Zemax_to_Speos.py
    
    Update the code to make functions more flexible for the speos zemax conversion
    
    * Update Convert_BSDF_Zemax_to_Speos.py
    
    Correction to read the header when there are empty lines after comments
    
    * Update Convert_BSDF_Zemax_to_Speos.py
    
    Added some comments
    
    * Run pre-commit
    
    * Update Convert_BSDF_Zemax_to_Speos.py
    
    * Update Convert_BSDF_Zemax_to_Speos.py
    
    Made some updates but there is still an issue with the BTDF data when going from Speos --> Zemax
    
    * Change the code to use a class
    
    - zemax to speos: working
    - speos to zemax: still fixing bugs
    
    * Removed obsolete functions + pre-commit corrections
    
    * Update BSDF_converter.py
    
    * Modification to get only one theta/phi/bsdf once the data is imported
    
    * Change bool_success to raise error when issues with data
    
    * Added import anisotropicbsdf
    
    * Corrected a mistake when converting speos --> zemax for anisotropic bsdf
    
    * Pre-commit correction
    
    * add new functions
    
    * Remove the anisotropy vector restriction
    
    * Correction on BSDF / intensity type
    
    * Pre-commit modifications
    
    * Modify Zemax --> Speos unit test for new code
    
    * Pre-commit update
    
    * Simplified code
    
    * Update BSDF_converter_example.py
    
    * Update dpf_brdf_viewer.py
    
    * Update tests numbe
    
    * Create test_13_anisotropicbsdf_viewer_zemax.anisotropicbsdf
    
    * Update to test13
    
    * Update to test13
    
    * Pre-commit update
    
    ---------
    
    Co-authored-by: plu <plu@ansys.com>
    Co-authored-by: StefanThoene <stefan.thoene@ansys.com>

---
+++ ansys_optical_automation/application/BSDF_converter_example.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+from tkinter import filedialog
+
+# import sys
+# repo_path="your_dir"
+# sys.path.append(repo_path)
+from ansys_optical_automation.interop_process.BSDF_converter import BsdfStructure
+
+
+def getfilename(extension, save=False):
+    """
+    Parameters
+    ----------
+    extension : str
+        containing the which file extension in *.ending format
+    save : Bool
+        option to define to open(default) or save. The default is False.
+
+    Returns
+    -------
+    str
+        string containing the selected file path
+    """
+    root = tk.Tk()
+    root.withdraw()
+    if not save:
+        file_path = filedialog.askopenfilename(filetypes=[("BSDF", extension)])
+    else:
+        file_path = filedialog.asksaveasfilename(filetypes=[("BSDF", extension)])
+        if not file_path.endswith(extension.strip("*")):
+            file_path += extension.strip("*")
+    return file_path
+
+
+def main():
+    """Main script to convert BSDF files"""
+
+    bool_log = 1
+    bsdf_data = BsdfStructure()
+    bsdf_data.filename_input = getfilename("*.bsdf *.brdf *.anisotropicbsdf")
+    print("The file to convert is: " + bsdf_data.filename_input)
+
+    msg = (
+        "Select the output format \n "
+        "1 = .bsdf (Zemax) \n "
+        "2 = .brdf (Speos) - Not yet supported \n "
+        "3 = .anisotropicbsdf\n"
+        "Enter >> "
+    )
+    output_choice = int(input(msg))
+
+    if not (output_choice in [1, 3]):
+        msg = "Wrong output format"
+        raise TypeError(msg)
+    else:
+        bsdf_data.import_data(bool_log)
+        if output_choice == 1:
+            bsdf_data.write_zemax_file(bool_log)
+        if output_choice == 3:
+            bsdf_data.write_speos_anisotropicbsdf_file()
+
+
+main()
---
+++ tests/workflows/run_test_13_anisotropicbsdf_viewer.py
@@ -0,0 +1,20 @@
+import os
+
+from ansys_optical_automation.interop_process.BSDF_converter import BsdfStructure
+
+
+def unittest_run():
+    """
+    main function to run conversion.
+
+    Returns
+    -------
+
+
+    """
+    bsdf_data = BsdfStructure()
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    bsdf_data.filename_input = os.path.join(cwd, "example_models", "test_13_anisotropicbsdf_viewer_zemax.bsdf")
+    # outputFilepath = os.path.join(cwd, "example_models", "test_12_anisotropicbsdf_viewer_speos.bsdf")
+    bsdf_data.import_data(0)
+    bsdf_data.write_speos_anisotropicbsdf_file()
