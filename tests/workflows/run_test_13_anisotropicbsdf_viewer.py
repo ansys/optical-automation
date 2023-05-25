4122be75d50681cf2ede4ad973ffabb053b0de89
SandAuriol <sandrine.auriol@ansys.com>
2023-05-25 09:32:02 +0100

    Memory issue test - deleting objects

--- tests/workflows/run_test_13_anisotropicbsdf_viewer.py
+++ tests/workflows/run_test_13_anisotropicbsdf_viewer.py
@@ -18,6 +18,7 @@ def unittest_run():
     bsdf_data.filename_input = os.path.join(cwd, "example_models", "test_13_planesymmetric_brdf_zemax.bsdf")
     bsdf_data.import_data(0)
     bsdf_data.write_speos_anisotropicbsdf_file()
+    del bsdf_data
 
     # Test 2
     bsdf_data = BsdfStructure()
@@ -25,6 +26,7 @@ def unittest_run():
     bsdf_data.filename_input = os.path.join(cwd, "example_models", "test_13_planesymmetric_btdf_zemax.bsdf")
     bsdf_data.import_data(0)
     bsdf_data.write_speos_anisotropicbsdf_file()
+    del bsdf_data
 
     # Test 3
     bsdf_data = BsdfStructure()
@@ -32,6 +34,7 @@ def unittest_run():
     bsdf_data.filename_input = os.path.join(cwd, "example_models", "test_13_asymmetrical4d_btdf_zemax.bsdf")
     bsdf_data.import_data(0)
     bsdf_data.write_speos_anisotropicbsdf_file()
+    del bsdf_data
 
     # Test 4
     bsdf_data = BsdfStructure()
@@ -39,6 +42,7 @@ def unittest_run():
     bsdf_data.filename_input = os.path.join(cwd, "example_models", "test_13_brdf_one_wavelength_speos.brdf")
     bsdf_data.import_data(0)
     bsdf_data.write_zemax_file(0)
+    del bsdf_data
 
     # Test 5
     bsdf_data = BsdfStructure()
@@ -46,3 +50,4 @@ def unittest_run():
     bsdf_data.filename_input = os.path.join(cwd, "example_models", "test_13_asymmetrical4d_brdf_speos.anisotropicbsdf")
     bsdf_data.import_data(0)
     bsdf_data.write_zemax_file(0)
+    del bsdf_data
