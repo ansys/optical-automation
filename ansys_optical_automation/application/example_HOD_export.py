6d3070a1fbdede015cde82f98922a45986142163
StefanThoene <stefan.thoene@ansys.com>
2023-07-31 14:28:39 +0200

    Fixed last bugs adjusted example missing: Unit tests

--- ansys_optical_automation/application/example_HOD_export.py
+++ ansys_optical_automation/application/example_HOD_export.py
@@ -1,12 +1,14 @@
 # Python Script, API Version = V21
-import sys
+# import sys
+# repo_path=r"your repository location"
+# sys.path.append(repo_path
+import os
 
-repo_path = r"D:\Work\Gitdir\Optical-automation"
-sys.path.append(repo_path)
 from ansys_optical_automation.speos_process.speos_hod import HOD
 
-hod_name = "HUD A"
-export_file_name = r"C:\temp"
-
+input_data = InputHelper.CreateTextBox("HUD A", "Name of HUD Design:", "Type name of HUD Design")
+InputHelper.PauseAndGetInput("Enter the HUD design name", input_data)
+export_file_name = os.path.split(GetActivePart().Document.Path)[0]
+hod_name = input_data.Value
 current_HOD_system = HOD(hod_name, SpeosDes, SpaceClaim)
 current_HOD_system.export_to_zemax(export_file_name, True)
