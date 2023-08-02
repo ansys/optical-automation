89605f9d9bba9747881ae41c64eb2f71f6315910
StefanThoene <stefan.thoene@ansys.com>
2023-08-02 08:24:08 +0200

    add xmp import example: example_reading_xmp_txt.py

--- ansys_optical_automation/application/example_reading_xmp_txt.py
+++ ansys_optical_automation/application/example_reading_xmp_txt.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 
 from ansys_optical_automation.post_process.dpf_xmp_viewer import DpfXmpViewer
@@ -35,10 +37,25 @@ def getfilename(extension, save=False):
     return file_path
 
 
-file_dir = getfilename("*.txt")
+# file_dir = getfilename("*.txt")
+# file_dir = r"path to xmp"
+file_dir = r"C:\temp\mytest_xmp_7.xmpexport.extended.txt_001.txt"
 xmp = DpfXmpViewer()
 data = xmp.read_txt_export(file_dir, inc_data=True)
-test = data.data.reshape((5, 5, 81))
-print(data.wl_res)
-print(test[:, :, 0])
-print(np.sum(test * 5, 2).transpose())
+# if there is one layer just reshape if there are mor sum them
+if data.data.shape[0] == 1:
+    test = data.data.reshape(data.data.shape[1:])
+else:
+    test = np.sum(data.data, 0)
+
+# if not spectral just reshape if ther is to simple integration
+if data.data.shape[3] == 1:
+    test = test.reshape(data.data.shape[1:2])
+else:
+    factor = (data.wl_res[1] - data.wl_res[0]) / (data.wl_res[2] - 1)
+    plot_data = np.sum(test * factor, 2).transpose()
+# plot data in grayscale to compare to speos result select grayscale in speos and adjust maximum
+max_scale = 2000
+plt.imshow(plot_data, cmap=mpl.colormaps["gray"], vmin=0, vmax=max_scale)
+plt.colorbar()
+plt.show()
