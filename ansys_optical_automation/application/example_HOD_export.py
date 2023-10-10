dc989b8fc63d592c02ad39cc26138e171312e6b7
StefanThoene <stefan.thoene@ansys.com>
2023-10-10 13:11:42 +0200

    remove print and log

--- ansys_optical_automation/application/example_HOD_export.py
+++ ansys_optical_automation/application/example_HOD_export.py
@@ -34,8 +34,6 @@ while not_selected:
 dir = os.path.split(GetActivePart().Document.Path)[0]
 name = os.path.split(GetActivePart().Document.Path)[1].rstrip("*.scdocx")
 export_file_path = os.path.join(dir, r"SPEOS output files", name)
-print(dir + r"\n" + name + r"\n" + export_file_path)
-
 current_HOD_system = HOD(hod_name, SpeosDes, SpaceClaim)
-current_HOD_system.export_to_zemax(export_file_path, True)
+current_HOD_system.export_to_zemax(export_file_path, False)
 current_HOD_system.export_ws(export_file_path)
