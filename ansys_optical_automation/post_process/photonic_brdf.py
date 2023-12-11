f681be356bea4e2ca3666665cc28fb8be15508b3
plu <pengyuan.lu@ansys.com>
2023-12-11 17:07:15 +0000

    add normalization based on first incident angle method

--- ansys_optical_automation/post_process/photonic_brdf.py
+++ ansys_optical_automation/post_process/photonic_brdf.py
@@ -36,6 +36,7 @@ class BrdfStructure:
         self.__theta_1d_ressampled = None
         self.__phi_1d_ressampled = None
         self.measurement_2d_brdf = []
+        self.rt_list = []
         self.brdf = []
         self.reflectance = []
         self.file_name = "export_brdfstruct"
@@ -96,10 +97,7 @@ class BrdfStructure:
         """
         theta_rad, phi_rad = np.radians(theta), np.radians(phi)  # samples on which integrande is known
         integrande = (1 / math.pi) * brdf * np.sin(theta_rad)  # *theta for polar integration
-        # deprecated scipy method
-        # f = interpolate.interp2d(theta_rad, phi_rad, integrande, kind="linear", bounds_error=False, fill_value=0)
         # calculation of integrande as from samples
-        # r = nquad(f, [[0, math.pi / 2], [0, 2 * math.pi]], opts=[{"epsabs": 0.1}, {"epsabs": 0.1}])
         # current scipy method
         f = interpolate.RectBivariateSpline(phi_rad, theta_rad, integrande, kx=1, ky=1)
         r = nquad(f, [[0, 2 * math.pi], [0, math.pi / 2]], opts=[{"epsabs": 0.1}, {"epsabs": 0.1}])
@@ -141,9 +139,6 @@ class BrdfStructure:
 
             """
             # function that calculated 2d brdf from 1d using revolution assumption.
-            # angular_distance = np.sqrt(
-            #     (incidence - theta * np.cos(np.radians(phi))) ** 2 + (theta * np.sin(np.radians(phi))) ** 2
-            # )
             angular_distance = np.sqrt(
                 (theta * np.cos(np.radians(phi)) - incidence) ** 2 + ((theta * np.sin(np.radians(phi))) / ratio) ** 2
             )
@@ -162,7 +157,8 @@ class BrdfStructure:
                 if measurement.incidence not in self.__incident_angles:
                     self.__incident_angles.append(measurement.incidence)
         # mesh grid for direct 2d matrix calculation
-        for incidence in self.__incident_angles:
+        normalized_scale = 1
+        for incidence_id, incidence in enumerate(self.__incident_angles):
             for wavelength in self.__wavelengths:
                 brdf_1d, theta_max = self.brdf_1d_function(wavelength, incidence)
                 self.__theta_1d_ressampled = np.linspace(0, 90, int(90 / sampling + 1))
@@ -171,9 +167,16 @@ class BrdfStructure:
                     self.__theta_1d_ressampled, self.__phi_1d_ressampled
                 )
                 self.brdf.append(brdf_2d_function(theta_2d_ressampled, phi_2d_ressampled))
+                if incidence_id == 0:
+                    normalized_scale = self.rt_list[0] / self.__brdf_reflectance(
+                        self.__theta_1d_ressampled, self.__phi_1d_ressampled, self.brdf[-1]
+                    )
+                self.brdf[-1] *= normalized_scale
                 self.reflectance.append(
                     self.__brdf_reflectance(self.__theta_1d_ressampled, self.__phi_1d_ressampled, self.brdf[-1])
                 )
+                print(self.rt_list[0], incidence, self.reflectance[-1])
+
         self.brdf = np.reshape(
             self.brdf,
             (
@@ -189,7 +192,7 @@ class BrdfStructure:
         self.brdf = np.moveaxis(self.brdf, 2, 3)
         self.reflectance = np.reshape(self.reflectance, (len(self.__incident_angles), len(self.__wavelengths)))
 
-    def export_to_speos(self, export_dir, rt="T"):
+    def export_to_speos(self, export_dir, rt):
         """
         save the 4d brdf structure into a brdf file.
 
@@ -269,7 +272,7 @@ class BrdfStructure:
         export.close()
 
 
-def convert_export(input_csv, output_path):
+def convert_export(input_csv, rt, rt_type, output_path):
     """
     main function to convert 2d data into brdf and export.
 
@@ -277,6 +280,10 @@ def convert_export(input_csv, output_path):
     ----------
     input_csv : str
         path of csv file recording the 2d data.
+    rt : str
+        path for rt txt file.
+    rt_type : str
+        type for transmission, R for reflection, T for transmission
     output_path : str
         path for the brdf to be exported.
 
@@ -287,6 +294,12 @@ def convert_export(input_csv, output_path):
     """
     wavelength_list = []
     brdf_data = None
+    RT = []
+    with open(rt) as myfile:
+        for line in myfile:
+            if "AOI" in line:
+                RT_value = line.split()[2:]
+                RT.append([float(value) * 100 for value in RT_value])
     with open(input_csv) as file:
         content = csv.reader(file, delimiter=",")
         FirstRow = True
@@ -301,11 +314,16 @@ def convert_export(input_csv, output_path):
                         BrdfMeasurementPoint(float(row[0]), wavelength, float(row[1]), float(row[2 + wavelength_idx]))
                     )
             FirstRow = False
+        if rt_type.lower() == "t":
+            brdf_data.rt_list = [item[1] for item in RT]
+        else:
+            brdf_data.rt_list = [item[0] for item in RT]
         brdf_data.convert(sampling=1)
-        brdf_data.export_to_speos(output_path)
+        brdf_data.export_to_speos(output_path, rt_type)
 
 
 cwd = os.path.realpath(__file__)
 repo = os.path.dirname(os.path.dirname(os.path.dirname(cwd)))
 measurement_2d_file = r"D:\Customer\CP\infinite\MakrolonDQ5122_speos_transmissiononly_modified.csv"
-convert_export(measurement_2d_file, r"D:\\")
+rt_file = r"D:\Customer\CP\infinite\RTfile.txt"
+convert_export(measurement_2d_file, rt_file, "T", r"D:\\")
